23d191fc1652f4d468c9c6a802fbeb89008f4792
Rafael Da Trindade <rafael.da.trindade@avanade.com>
2020-04-14 16:19:52 -0300

    projeto criado e firebase configurado

---
+++ crud/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ crud/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCfO0hUu-6DTrPvPnR5DaQH_SYMXltOLhU',
+    authDomain: 'crud-loja.firebaseapp.com',
+    databaseURL: 'https://crud-loja.firebaseio.com',
+    projectId: 'crud-loja',
+    storageBucket: 'crud-loja.appspot.com',
+    messagingSenderId: '117948226562',
+    appId: '1:117948226562:web:458a41461ea00188b49b1b',
+    measurementId: 'G-TME3N1X5J0',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
